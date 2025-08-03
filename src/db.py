7a00729e3cc4ac34f55517889f3736368ffb55e8
CovertCloak <CovertCloak06@proton.me>
2025-08-03 14:32:26 -0700

    feat: add SQLite persistence scaffold

---
+++ src/db.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, Text, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+engine = create_engine('sqlite:///conversations.db', echo=False)
+Session = sessionmaker(bind=engine)
+
+class Conversation(Base):
+    __tablename__ = 'conversations'
+    id = Column(Integer, primary_key=True)
+    prompt = Column(Text)
+    response = Column(Text)
+
+Base.metadata.create_all(engine)
